042f65e74b4dfcd0cd8ba3ce8f22db869e91ae66
Laurent Granié <lgranie@users.noreply.github.com>
2026-05-15 16:52:21 +0000

    [base] Init krw mise tasks with gpg

--- files/base/tools/etc/fish/functions/gpg-sign-pub.fish
+++
@@ -1,9 +0,0 @@
-function gpg-sign-pub --description 'Extract gpg sign pub key'
-    set gpg_key_id $(gpg2 --list-secret-keys --keyid-format LONG | grep ssb | sed -E 's/.*\/([0-9A-Z]*) .*\[.*S.*\]/\1/')
-    
-    set gpg_pubkey $(gpg --armor --export $gpg_key_id)
-    echo "Your gpg pub key is"
-    echo $gpg_pubkey
-
-    gum confim "Copy to clipboard?" && echo $gpg_pubkey | wl-copy
-end
