01f7c947ac6215146c0dd25370d8e7be1fdd17ae
Laurent Granié <laurent.granie@proton.me>
2026-05-22 10:47:17 +0200

    [voxtype] Unmute mute around recording

--- files/wm/common/voxtype/usr/share/hypr/additionals/voxtype.lua
+++ files/wm/common/voxtype/usr/share/hypr/additionals/voxtype.lua
@@ -1,4 +1,4 @@
 local exec = hl.dsp.exec_cmd
 
-hl.bind("SUPER + D", exec("voxtype record start"), { description = "Start Voxtype record" })
-hl.bind("SUPER + SHIFT + D", exec("voxtype record stop"), { description = "Stop Voxtype record" })
+hl.bind("SUPER + D", exec("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 0; voxtype record start"), { description = "Start Voxtype record" })
+hl.bind("SUPER + SHIFT + D", exec("voxtype record stop; wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 1"), { description = "Stop Voxtype record" })
