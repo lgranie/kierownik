aa45c9998e118b8a364c83566b5de4b2e7ee0b03
Laurent Granié <laurent.granie@proton.me>
2026-05-31 14:50:32 +0200

    [hypr] Remove 2in1

--- files/wm/hypr/usr/share/hypr/additionals.lua
+++ files/wm/hypr/usr/share/hypr/additionals.lua
@@ -1,6 +1,6 @@
-local success, err = pcall(function()
-    require("additionals/2in1")
-end)
+-- local success, err = pcall(function()
+--     require("additionals/2in1")
+-- end)
 
 local success, err = pcall(function()
     require("additionals/voxtype")
