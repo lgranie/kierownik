177daf032d403fd449f838c2a2be098f2b1e97f1
Laurent Granié <laurent.granie@proton.me>
2026-05-31 15:25:35 +0200

    [audio] Link to alsa

--- files/wm/common/audio/usr/share/wireplumber/scripts/auto-connect-ports.lua
+++ files/wm/common/audio/usr/share/wireplumber/scripts/auto-connect-ports.lua
@@ -198,7 +198,7 @@ end
 -- Auto connect Cardinal 2in1
 auto_connect_ports {
   output = Constraint { "object.path", "matches", "Cardinal:output_*" },
-  input = Constraint { "port.alias", "matches", "ALC3253 Analog:playback_*" },
+  input  = Constraint { "object.path", "matches", "alsa:acp:*" },
   connect = {
     ["audio_out_1"] = "playback_FL",
     ["audio_out_2"] = "playback_FR",
