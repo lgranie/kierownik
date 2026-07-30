9737368a2cd4989b9eae8e1f524c82a7e4fc8064
Laurent Granié <laurent.granie@proton.me>
2026-07-30 17:47:12 +0200

    [fish] Share SSH_AUTH_SOCK with systemd

--- files/base/tools/etc/fish/conf.d/10-gpg.fish
+++ files/base/tools/etc/fish/conf.d/10-gpg.fish
@@ -2,5 +2,6 @@
 
 set -gx GPG_TTY (tty)
 set -U -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+systemctl --user set-environment SSH_AUTH_SOCK=$SSH_AUTH_SOCK
 
 gpg-connect-agent updatestartuptty /bye > /dev/null
