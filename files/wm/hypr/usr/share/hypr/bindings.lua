127f701b721eeb7417875459e094148a4e2d43fd
Laurent Granié <laurent.granie@proton.me>
2026-08-02 17:54:16 +0200

    [hypr] Replace tmux with herdr

--- files/wm/hypr/usr/share/hypr/bindings.lua
+++ files/wm/hypr/usr/share/hypr/bindings.lua
@@ -21,7 +21,7 @@ hl.bind("SUPER + CTRL + V", exec("cliphist decode | wl-paste"))
 
 -- 1. Applications
 hl.bind("SUPER + RETURN", exec("uwsm app -- footclient"), { description = "Open a Terminal: foot client" })
-hl.bind("SUPER + ALT + RETURN", exec("uwsm app -- footclient tmux"), { description = "Open a Terminal: foot client tmux" })
+hl.bind("SUPER + ALT + RETURN", exec("uwsm app -- footclient herdr"), { description = "Open a Terminal: foot client Herdr" })
 hl.bind("SUPER + SHIFT + RETURN", exec("foot"), { description = "Open a Terminal: foot" })
 hl.bind("SUPER + SHIFT + SPACE", exec(ipc .. "panel-toggle launcher"), { description = "Run an Application: Menu" })
 hl.bind("SUPER + SPACE", exec("footclient --title=fsel fsel --config /etc/fsel/config.toml --detach"), { description = "Run an Application via fsel" })
