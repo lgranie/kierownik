0e8013ac673d937803d63cd52761756ffba86ae2
Laurent Granié <laurent.granie@proton.me>
2026-09-17 15:39:46 +0200

    [hypr] Set env

--- files/wm/common/hypr/usr/share/hypr/autostart.lua
+++ files/wm/common/hypr/usr/share/hypr/autostart.lua
@@ -8,7 +8,7 @@
 -- Or execute your favorite apps at launch like this:
 
 hl.on("hyprland.start", function()
-	hl.exec_cmd("dbus-update-activation-environment --all")
+	hl.exec_cmd("dbus-update-activation-environment --systemd --all")
 	hl.exec_cmd("exec /usr/libexec/pam_kwallet_init")
 end)
 
